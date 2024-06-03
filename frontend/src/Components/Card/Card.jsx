1a6a89930f67907a64d2e9692635efdfc0e7c32d
Sammit <poudyal.sammit@gmail.com>
2024-06-04 00:00:43 +0545

    added url for a specific job

--- frontend/src/Components/Card/Card.jsx
+++ frontend/src/Components/Card/Card.jsx
@@ -5,7 +5,7 @@ import { FaRegClock } from "react-icons/fa";
 import rupees from '../../Assets/rupees.png';
 import { ImCalendar } from "react-icons/im";
 
-
+ const url= "https://internsathi.com/_next/image?url=https%3A%2F%2Fapi.internsathi.com%2Fuploads%2F1704219829366-277175250.png&w=256&q=75";
 const Card = ({data}) => {
 
     const {companyName,jobTitle,companyLogo,minPrice,maxPrice,salaryType,jobLocation,postingDate,employmentType,description}= data;
@@ -13,7 +13,13 @@ const Card = ({data}) => {
     return (
         <section className='card bg-[#eefffe] hover:bg-green-200 transition-colors duration-300 ease-out delay-120 transform motion-safe:hover:scale-105'>
             <Link to={"/"} className="flex gap-4 flex-col sm:flex-row items-start">
-                <img src={companyLogo} alt='' />
+            {
+                companyName.toLowerCase() === "Talentsathi".toLowerCase() ? (
+                    <img src={url} alt='' className='w-[72px] h-[72px]' />
+                ) : (
+                    <img src={companyLogo} alt='' />
+                )
+            }
                 <div>
                     <h4 className='text-Text mb-1'>{companyName}</h4>
                     <h3 className='text-lg font-semibold mb-2'>{jobTitle}</h3>
