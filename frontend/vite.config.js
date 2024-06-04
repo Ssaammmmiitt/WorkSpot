b14e5e403e8c21684af2dc093b2fb81925b0cf98
Sammit <poudyal.sammit@gmail.com>
2024-06-04 23:54:41 +0545

    backend not working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path';
 
-const root = resolve(dirname);
-const outDir = resolve(dirname, 'dist');
+const root = resolve(__dirname);
+const outDir = resolve(__dirname, 'dist');
 
 
 // https://vitejs.dev/config/
--- server/App.js
+++ server/App.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
     
     }
 
