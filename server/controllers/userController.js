2a89dca086babcf9985ad3f19a057a38ca1d2360
kadel-28 <ashrayakadel28@gmail.com>
2024-06-05 23:00:34 +0545

    update JWT features

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 
 
 const createToken = (_id) => {
-    return jwt.sign({_id:_id}, process.env.SECRET, {expiresIn :'1d'})
+    return jwt.sign({_id:_id}, process.env.SECRET, {expiresIn :'3h'})
 }
 
 //login user
