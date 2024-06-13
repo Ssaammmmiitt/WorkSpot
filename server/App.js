3308c9a190af1207a9b7c0ae52323d7dc1082073
kadel-28 <ashrayakadel28@gmail.com>
2024-06-13 09:52:19 +0545

    #mix

---
+++ server/App.js
@@ -0,0 +1,22 @@
+port=5173;
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+    
+    }
+
+);
+
+app.listen(port, () => {
+
+    console.log(`Server is running on port ${port}`);
+
+}
+);
