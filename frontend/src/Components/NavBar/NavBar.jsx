81fe0058183f4f27a3db95da149c8d3db190c654
Akash kafle <aakashkafle47@gmail.com>
2024-06-15 14:19:54 +0545

    signout added

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -306,6 +306,7 @@ const NavBar = () => {
   const handleSignout = () => {
     logout();
     setIsLoggedIn(false);
+    NavBar();
   };
 
 
@@ -362,12 +363,12 @@ const NavBar = () => {
                 My Jobs
               </Link>
               <Link
-
+                onClick={handleSignout}
                 to="/login"
                 className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-base font-semibold text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
               >
 
-                <button onClick={handleSignout} className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <button className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Sign Out
                 </button>
               </Link>
