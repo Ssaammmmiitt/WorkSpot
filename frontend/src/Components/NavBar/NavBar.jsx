e936e3b427ef644465870c64327fe5c44648d174
Sammit <poudyal.sammit@gmail.com>
2024-06-16 01:02:58 +0545

    updated dropdown

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,15 +1,37 @@
 import Swal from "sweetalert2";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
 import search from "../../Assets/search.json";
 import Lottie from "lottie-react";
+import { BiLogOut } from "react-icons/bi";
 import { useAuth } from "../../firebase/AuthProvider";
+import { getAuth } from "firebase/auth";
+import { FaChevronDown } from "react-icons/fa";
 
 const NavBar = () => {
   const { user, logout } = useAuth();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(user != null); // Set initial state to false for demonstration
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoUrl, setPhotoUrl] = useState("");
+  const [firstName, setFirstName] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      const result = getAuth().currentUser;
+      setPhotoUrl(result.photoURL);
+      setFirstName(result.displayName.split(" ")[0]);
+    }
+  }, [user]);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
 
   const handleMenuToggler = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -54,24 +76,6 @@ const NavBar = () => {
     }
   };
 
-  // const onSignOut=  (e,path) => {
-  //   e.preventDefault();
-  //   Swal.fire({
-  //     title: "Do you want to SignOut? This will take you to login page",
-  //     showCancelButton:true,
-  //     confirmButtonText:"SignOut",
-  //   }).then((result)=>
-  //   {
-  //     if (result.isConfirmed)
-  //       {
-  //         logout();
-  //   setIsLoggedIn(false);
-  //   NavBar();
-  // };
-  //       }
-  //     )
-  //   };
-
   const onSignOut = (e) => {
     e.preventDefault();
     Swal.fire({
@@ -82,179 +86,20 @@ const NavBar = () => {
       if (result.isConfirmed) {
         logout();
         setIsLoggedIn(false);
-        NavBar();  // This may not be necessary as the state change should re-render the component
+        NavBar(); // This may not be necessary as the state change should re-render the component
       }
     });
   };
-
-  // const handleSignout = () => {
-  //   logout();
-  //   setIsLoggedIn(false);
-  //   NavBar();
-  // };
-
-
-  //   return (<>
-  //     {/*handleLogin*/}
-  //     < header className="max-w-screen-2xl container mx-auto xl:px-24 px-4" >
-  //       <nav className="flex justify-between items-center py-6">
-  //         <div className="flex items-center justify-center">
-  //           <Lottie animationData={search} style={{ width: 100, height: 100 }} />
-  //           {/* <Link to={isLoggedIn ? "/" : ""}> <h1 className='logo text-[35px] animate-bounce text-black font-bold hover:text-Primary'><strong>Work</strong>Spot</h1></Link> */}
-  //           {isLoggedIn ? (
-  //             <Link to="/">
-  //               <h1 className="logo text-[35px] animate-bounce text-black font-bold hover:text-Primary">
-  //                 <strong>Work</strong>Spot
-  //               </h1>
-  //             </Link>
-  //           ) : (
-  //             <h1 className="logo text-[35px] animate-bounce text-black font-bold hover:text-Primary">
-  //               <strong>Work</strong>Spot
-  //             </h1>
-  //           )}
-  //         </div>
-
-  //         {/* nav items for large devices */}
-  //         <ul className="hidden md:flex gap-8">
-  //           {navItems.map(({ path, title }) => (
-  //             <li
-  //               key={path}
-  //               className="text-lg font-medium text-Text no-underline hover:underline"
-  //             >
-  //               {title == "Start a search" || title == "Estimated Salary" ? (
-  //                 <NavLink
-  //                   to={path}
-  //                   className={({ isActive }) => (isActive ? "active" : "")}
-  //                   onClick={(e) => handleProtectedNavClick(e, path)}
-  //                 >
-  //                   {title}
-  //                 </NavLink>
-  //               ) : (
-  //                 <Link to={path}>{title}</Link>
-  //               )}
-  //             </li>
-  //           ))}
-  //         </ul>
-
-  //         {/* signup and login button */}
-  //         <div className="hidden md:flex items-center space-x-4">
-  //           {isLoggedIn ? (
-  //             <>
-  //               <Link
-  //                 to="/app/my-jobs"
-  //                 className="text-lg font-medium text-Text no-underline hover:underline"
-  //               >
-  //                 My Jobs
-  //               </Link>
-  //               <Link
-  //                 onClick={(e)=> onSignOut(e,path)}
-  //                 to="/login"
-  //                 className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-base font-semibold text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-  //               >
-
-  //                 <button className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-  //                   Sign Out
-  //                 </button>
-  //               </Link>
-  //             </>
-  //           ) : (
-  //             <>
-  //               <Link
-  //                 to="/login"
-  //                 className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-base font-semibold text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-  //               >
-  //                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-  //                   Login
-  //                 </span>
-  //               </Link>
-  //               <Link
-  //                 to="/sign-up"
-  //                 className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-base font-semibold text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-  //               >
-  //                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-  //                   Sign Up
-  //                 </span>
-  //               </Link>
-  //             </>
-  //           )}
-  //         </div>
-
-  //         {/* mobile menu button */}
-  //         <div className="md:hidden block">
-  //           <button onClick={handleMenuToggler}>
-  //             {isMenuOpen ? (
-  //               <FaXmark className="h-5 w-5" />
-  //             ) : (
-  //               <FaBarsStaggered className="w-5 h-5 text-Text" />
-  //             )}
-  //           </button>
-  //         </div>
-  //       </nav>
-
-  //       {/* nav items for mobile */}
-  //       <div
-  //         className={`px-4 bg-black py-5 rounded-sm ${isMenuOpen ? "" : "hidden"
-  //           }`}
-  //       >
-  //         <ul className="flex flex-col items-center space-y-4">
-  //           {/* {navItems.map(({ path, title }) => (
-  //             <li key={path} className='text-base text-white py-1 text-center'>
-  //               <NavLink
-  //                 to={path}
-  //                 className={({ isActive }) => isActive ? "active" : ""}
-  //                 onClick={(e) => handleProtectedNavClick(e, path)}
-  //               >
-  //                 {title}
-  //               </NavLink>
-  //             </li>
-  //           ))} */}
-  //           {navItems.map(({ path, title }) => (
-  //             <li key={path} className="text-base text-white py-1 text-center">
-  //               {title == "Start a search" || title == "Estimated Salary" ? (
-  //                 <NavLink
-  //                   to={path}
-  //                   className={({ isActive }) => (isActive ? "active" : "")}
-  //                   onClick={(e) => handleProtectedNavClick(e, path)}
-  //                 >
-  //                   {title}
-  //                 </NavLink>
-  //               ) : (
-  //                 <Link to={path}>{title}</Link>
-  //               )}
-  //             </li>
-  //           ))}
-  //           <li className="text-white">
-  //             <Link
-  //               to="/login"
-  //               className="text-white block text-center md:text-left"
-  //             >
-  //               Login
-  //             </Link>
-  //           </li>
-  //           <li className="text-white">
-  //             <Link
-  //               to="/sign-up"
-  //               className="text-white block text-center md:text-left"
-  //             >
-  //               Sign Up
-  //             </Link>
-  //           </li>
-  //         </ul>
-  //       </div>
-  //     </header >
-  //   </>
-  //   );
-  // };
-
-  // export default NavBar;
-
 
   return (
     <>
       <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
         <nav className="flex justify-between items-center py-6">
           <div className="flex items-center justify-center">
-            <Lottie animationData={search} style={{ width: 100, height: 100 }} />
+            <Lottie
+              animationData={search}
+              style={{ width: 100, height: 100 }}
+            />
             {isLoggedIn ? (
               <Link to="/app">
                 <h1 className="logo text-[35px] animate-bounce text-black font-bold hover:text-Primary">
@@ -291,23 +136,63 @@ const NavBar = () => {
 
           <div className="hidden md:flex items-center space-x-4">
             {isLoggedIn ? (
-              <>
-                <Link
-                  to="/app/my-jobs"
-                  className="text-lg font-medium text-Text no-underline hover:underline"
-                >
-                  My Jobs
-                </Link>
-                <Link
-                  onClick={(e) => onSignOut(e)}
-                  to="/login"
-                  className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-base font-semibold text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-                >
-                  <button className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    Sign Out
+              <div className="w-full py-6 pb-8">
+                <div className="relative inline-block">
+                  <button
+                    type="button"
+                    className="px-4 py-2 text-white bg-Primary hover:bg-Primary/65 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm inline-flex items-center"
+                    onClick={toggleDropdown}
+                  >
+                    {firstName}{" "}
+                    <img
+                      className="ml-4 rounded-full w-7 h-7 border"
+                      src={photoUrl}
+                    />{" "}
+                    <FaChevronDown className="w-2.5 h-2.5 ml-2.5" />
                   </button>
-                </Link>
-              </>
+
+                  {isOpen && (
+                    <div className="origin-top-right absolute right-0 mt-2 w-44 rounded-lg shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                      <ul
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="options-menu"
+                      >
+                        <li>
+                          <Link
+                            to="/profile"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                            onClick={closeDropdown}
+                          >
+                            My Profile
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            to="/app/my-jobs"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                            onClick={closeDropdown}
+                          >
+                            My Jobs
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            onClick={[(e) => onSignOut(e), closeDropdown]}
+                            to="/login"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          >
+                            <div className="flex items-center">
+                              <span className="mr-2">Sign Out</span>
+                              <BiLogOut className="" />
+                            </div>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </div>
             ) : (
               <>
                 <Link
@@ -341,7 +226,11 @@ const NavBar = () => {
           </div>
         </nav>
 
-        <div className={`px-4 bg-black py-5 rounded-sm ${isMenuOpen ? "" : "hidden"}`}>
+        <div
+          className={`px-4 bg-black py-5 rounded-sm ${
+            isMenuOpen ? "" : "hidden"
+          }`}
+        >
           <ul className="flex flex-col items-center space-y-4">
             {navItems.map(({ path, title }) => (
               <li key={path} className="text-base text-white py-1 text-center">
@@ -359,12 +248,18 @@ const NavBar = () => {
               </li>
             ))}
             <li className="text-white">
-              <Link to="/login" className="text-white block text-center md:text-left">
+              <Link
+                to="/login"
+                className="text-white block text-center md:text-left"
+              >
                 Login
               </Link>
             </li>
             <li className="text-white">
-              <Link to="/sign-up" className="text-white block text-center md:text-left">
+              <Link
+                to="/sign-up"
+                className="text-white block text-center md:text-left"
+              >
                 Sign Up
               </Link>
             </li>
--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -6,7 +6,7 @@ import { CgWebsite } from "react-icons/cg";
 import { MdEmail } from "react-icons/md";
 import { IoLocationSharp } from "react-icons/io5";
 import { PiHandbagSimpleFill } from "react-icons/pi";
-
+import { FaPhoneAlt } from "react-icons/fa";
 
 
 
@@ -21,20 +21,22 @@ const Profile = () => {
 
 		<div className="p-4 md:p-12 text-center lg:text-left">
 			{/*<!-- Image for mobile view--> */}
-			<div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center bg-('https://source.unsplash.com/MP0IUfwrn0A')"></div>
+			<img className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center bg-[url('https://cdn-icons-png.flaticon.com/512/149/149071.png')]"/>
 			<h1 className="text-3xl font-bold pt-8 lg:pt-0">Your Name</h1>
 			<div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
-			<p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><PiHandbagSimpleFill className='mr-5 w-7 h-7'/> What you do</p>
+			<p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-briefcase mr-5 w-7 h-7">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  <path d="M22 13.478v4.522a3 3 0 0 1 -3 3h-14a3 3 0 0 1 -3 -3v-4.522l.553 .277a20.999 20.999 0 0 0 18.897 -.002l.55 -.275zm-8 -11.478a3 3 0 0 1 3 3v1h2a3 3 0 0 1 3 3v2.242l-1.447 .724a19.002 19.002 0 0 1 -16.726 .186l-.647 -.32l-1.18 -.59v-2.242a3 3 0 0 1 3 -3h2v-1a3 3 0 0 1 3 -3h4zm-2 8a1 1 0 0 0 -1 1a1 1 0 1 0 2 .01c0 -.562 -.448 -1.01 -1 -1.01zm2 -6h-4a1 1 0 0 0 -1 1v1h6v-1a1 1 0 0 0 -1 -1z" />
+</svg> What you do</p>
 			<p className="pt-2 text-Text text-xs lg:text-sm flex items-center justify-center lg:justify-start"><IoLocationSharp className='mr-5 w-7 h-7'/> Your Location - 25.0000° N, 71.0000° W</p>
             <p className="pt-2 text-Text text-xs lg:text-sm flex items-center justify-center lg:justify-start"><MdEmail className=' mr-5 w-7 h-7'/> Your email</p>
+            <p className="pt-2 text-Text text-xs lg:text-sm flex items-center justify-center lg:justify-start"><FaPhoneAlt className=' mr-5 w-6 h-6'/> Your phone</p>
 			<p className="pt-8 text-sm">Totally optional short description about yourself, what you do and so on.</p>
 
 			<div className="pt-12 pb-8">
-				<button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
-				  Get In Touch
-				</button> 
+				
 			</div>
-
+            <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
             <h1 className='text-[#000000]'>Urls:</h1>
 			<div className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-col ">
             <ul >
