ce5f16f7d99169a84309ab5d6d5c9762233a5ddc
Akash kafle <aakashkafle47@gmail.com>
2024-06-15 14:09:47 +0545

    protected works

--- frontend/src/Components/protected.jsx
+++ frontend/src/Components/protected.jsx
@@ -5,11 +5,5 @@ import { useAuth } from "../firebase/AuthProvider";
 
 export const ProtectedRoute = ({ children }) => {
     let { user, loading } = useAuth();
-    if (user) {
-        loading = false;
-    }
-    if (loading) {
-        return <div>Loading...</div>;
-    }
     return user ? children : <Navigate to="/login" />;
 };
